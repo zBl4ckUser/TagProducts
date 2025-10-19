57d1daedc4339728dde5fcaf5426431497f99a3c
zBl4ckUser <zBl4ckDev@protonmail.com>
2025-10-19 15:32:46 -0300

    feat: add cors to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/router';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './config/swagger.config';
@@ -6,6 +7,7 @@ import swaggerSpec from './config/swagger.config';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 
 app.use('/api', router);
